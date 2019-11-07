6f5474f78d9a75dbc6beb0ea8fb6712034f1ca61
Serge Sychov <sergesychov@MacBook-Air-Serge.local>
2019-11-07 20:22:59 +0300

    Initial Commit

---
+++ MyPurser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPurser
+//
+//  Created by Serge Sychov on 07.11.2019.
+//  Copyright © 2019 Serge Sychov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
